dc55460e5706b356dd35209c2612471161be6774
Dmitry_Rachko <dima229131@gmail.com>
2022-01-03 19:22:38 +0300

    fix forecast screen with safearea

--- lib/screens/forecast_screen/forecast_view.dart
+++ lib/screens/forecast_screen/forecast_view.dart
@@ -14,82 +14,87 @@ class _ForecastViewState extends State<ForecastView> {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
 
-    return Padding(
-      padding: EdgeInsets.all(
-        size.height * 0.005,
-      ),
-      child: ListView.builder(
-        padding: const EdgeInsets.symmetric(horizontal: 8),
-        itemCount: widget.snapshot.length,
-        itemBuilder: (BuildContext context, int index) {
-          return Card(
-            elevation: 5,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[
-                  Container(
-                    width: 80,
-                    height: 80,
-                    decoration: BoxDecoration(
-                        image: DecorationImage(
-                            image: NetworkImage(
-                                'http://openweathermap.org/img/w/${widget.snapshot[index].weatherIcon.toString()}.png'),
-                            fit: BoxFit.contain)),
-                  ),
-                  const SizedBox(width: 20),
-                  Expanded(
-                    flex: 4,
-                    child: SizedBox(
-                      height: 60,
-                      child: Column(
-                        children: <Widget>[
-                          Expanded(
-                            flex: 2,
-                            child: Text(
-                              DateFormat('EEEE, d MMM, yyyy' ' kk:mm:a').format(
-                                  DateTime.parse(
-                                      widget.snapshot[index].date.toString())),
-                              style: const TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.w600,
-                              ),
-                            ),
-                          ),
-                          Expanded(
-                            child: Text(
-                              widget.snapshot[index].weatherDescription
-                                  .toString(),
-                              style: const TextStyle(
-                                fontSize: 13,
-                                fontWeight: FontWeight.w600,
-                              ),
-                            ),
-                          ),
-                        ],
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(
+            size.height * 0.005,
+          ),
+          child: ListView.builder(
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            itemCount: widget.snapshot.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Card(
+                elevation: 5,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      Container(
+                        width: 80,
+                        height: 80,
+                        decoration: BoxDecoration(
+                            image: DecorationImage(
+                                image: NetworkImage(
+                                    'http://openweathermap.org/img/w/${widget.snapshot[index].weatherIcon.toString()}.png'),
+                                fit: BoxFit.contain)),
                       ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 30, horizontal: 10),
-                    child: Text(
-                      widget.snapshot[index].temperature.toString(),
-                      style: const TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.w600,
+                      const SizedBox(width: 20),
+                      Expanded(
+                        flex: 4,
+                        child: SizedBox(
+                          height: 60,
+                          child: Column(
+                            children: <Widget>[
+                              Expanded(
+                                flex: 2,
+                                child: Text(
+                                  DateFormat('EEEE, d MMM, yyyy' ' kk:mm:a')
+                                      .format(DateTime.parse(widget
+                                          .snapshot[index].date
+                                          .toString())),
+                                  style: const TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                              ),
+                              Expanded(
+                                child: Text(
+                                  widget.snapshot[index].weatherDescription
+                                      .toString(),
+                                  style: const TextStyle(
+                                    fontSize: 13,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
                       ),
-                    ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 30, horizontal: 10),
+                        child: Text(
+                          widget.snapshot[index].temperature.toString(),
+                          style: const TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                      const Divider(
+                        color: Colors.black,
+                      ),
+                    ],
                   ),
-                  const Divider(
-                    color: Colors.black,
-                  ),
-                ],
-              ),
-            ),
-          );
-        },
+                ),
+              );
+            },
+          ),
+        ),
       ),
     );
   }
